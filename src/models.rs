e8b3d286b6f63dd6ae069690086c682f091412e6
quanturtle <alvaromachucar@gmail.com>
2023-06-09 18:36:01 -0400

    added Region enum

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,23 @@
 use serde::{Deserialize, Serialize};
 
+enum Region {
+    US_EAST_1,
+    US_EAST_2,
+    US_WEST_1,
+    US_WEST_2
+}
+
+impl std::fmt::Display for Region {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Region::US_EAST_1 => write!(f, "us-east-1"),
+            Region::US_EAST_2 => write!(f, "us-east-2"),
+            Region::US_WEST_1 => write!(f, "us-west-1"),
+            Region::US_WEST_2 => write!(f, "us-west-2"),
+        }
+    }
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct User {
     id: String,
